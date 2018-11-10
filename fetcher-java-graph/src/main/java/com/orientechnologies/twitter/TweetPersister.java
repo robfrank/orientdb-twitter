39cdcdd2876eaddf253f81fa440b15d730066e6f
robfrank <ro.franchini@gmail.com>
2018-11-10 14:43:29 +0100

    removes verbose logging

--- fetcher-java-graph/src/main/java/com/orientechnologies/twitter/TweetPersister.java
+++ fetcher-java-graph/src/main/java/com/orientechnologies/twitter/TweetPersister.java
@@ -210,7 +210,6 @@ public class TweetPersister {
                     });
         }};
 
-        log.info("props:: {}", props);
         orientVertex.setProperties(props.toArray()).save();
 
         return orientVertex;
