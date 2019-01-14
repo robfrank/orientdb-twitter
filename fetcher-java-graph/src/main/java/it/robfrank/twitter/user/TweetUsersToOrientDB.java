95d10abf8be08df9829ef0217bded4f2e1961dba
robfrank <ro.franchini@gmail.com>
2019-01-14 10:03:27 +0100

    removes the use of the listener

--- fetcher-java-graph/src/main/java/it/robfrank/twitter/user/TweetUsersToOrientDB.java
+++ fetcher-java-graph/src/main/java/it/robfrank/twitter/user/TweetUsersToOrientDB.java
@@ -1,16 +1,15 @@
 package it.robfrank.twitter.user;
 
-import com.orientechnologies.orient.core.command.OCommandResultListener;
 import com.orientechnologies.orient.core.db.OPartitionedDatabasePool;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
-import it.robfrank.twitter.TweetPersister;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
 import com.tinkerpop.blueprints.impls.orient.OrientVertex;
+import it.robfrank.twitter.TweetPersister;
 import lombok.extern.log4j.Log4j2;
 import twitter4j.RateLimitStatus;
 import twitter4j.TwitterException;
@@ -29,8 +28,11 @@ public class TweetUsersToOrientDB {
 
     private final String dbUrl;
 
+    private final FriendsFollowersResources followersResources;
+
     public TweetUsersToOrientDB() {
         dbUrl = System.getProperty("tw2odb.dbUrl", "plocal:./tweets");
+        followersResources = TwitterFactory.getSingleton().friendsFollowers();
 
 
     }
@@ -41,12 +43,9 @@ public class TweetUsersToOrientDB {
         final OrientGraphFactory factory = new OrientGraphFactory(dbUrl, "admin", "admin")
                 .setupPool(1, 10);
 
-        final TweetPersister repository = new TweetPersister(factory);
+        final TweetPersister persister = new TweetPersister(factory);
 
-        OCommandResultListener listener = new UserResultListener(repository, factory);
-
-
-        OPartitionedDatabasePool pool = new OPartitionedDatabasePool(dbUrl, "admin", "admin");
+        final OPartitionedDatabasePool pool = new OPartitionedDatabasePool(dbUrl, "admin", "admin");
 
         while (true) {
             ODatabaseDocumentTx db = pool.acquire();
@@ -64,7 +63,7 @@ public class TweetUsersToOrientDB {
                 final List<ODocument> users = db.query(new OSQLSynchQuery<ODocument>("SELECT FROM User where fetched = false LIMIT 5"));
 
                 users.stream()
-                        .forEach(uer -> listener.result(uer));
+                        .forEach(uer -> result(uer, factory, persister));
 
 
             } catch (Exception e) {
@@ -74,8 +73,7 @@ public class TweetUsersToOrientDB {
                 db.close();
             }
         }
-//                pool.close();
-
+//        pool.close();
     }
 
     public void stop() {
@@ -83,117 +81,93 @@ public class TweetUsersToOrientDB {
     }
 
 
-    private static class UserResultListener implements OCommandResultListener {
+    public boolean result(ODocument record, OrientGraphFactory factory, TweetPersister persister) {
 
-        private final TweetPersister persister;
-        private final OrientGraphFactory factory;
-        private final FriendsFollowersResources followersResources;
+        OrientBaseGraph graph = factory.getTx();
 
-        public UserResultListener(TweetPersister persister, OrientGraphFactory factory) {
-            this.persister = persister;
-            this.factory = factory;
+        try {
+            final OrientVertex user = graph.getVertex(record);
 
-            followersResources = TwitterFactory.getSingleton().friendsFollowers();
-        }
+            if (user == null) return false;
 
-        @Override
-        public boolean result(Object record) {
+            user.setProperty("fetched", true, OType.BOOLEAN);
 
-            OrientBaseGraph graph = factory.getTx();
+            user.save();
+
+            log.info("getting followers and friends for user {} ", user);
+
+            Long userId = user.getProperty("userId");
 
             try {
-                final OrientVertex user = graph.getVertex(record);
+                final String followers = followersResources.getFollowersList(userId, -1, 200).stream()
+                        .map(follower -> {
+                                    Vertex followerVertex = persister.storeUser(graph, follower);
+                                    graph.commit();
 
-                if (user == null) return false;
+                                    final Iterable<ODocument> execute = graph.command(new OSQLSynchQuery<>("select from " + followerVertex.getId() + " where out('Follows').@rid contains " + user.getId()))
+                                            .execute();
 
-                user.setProperty("fetched", true, OType.BOOLEAN);
-
-                user.save();
-
-                log.info("getting followers and friends for user {} ", user);
-
-                Long userId = user.getProperty("userId");
-
-                try {
-                    final String followers = followersResources.getFollowersList(userId, -1, 200).stream()
-                            .map(follower -> {
-                                        Vertex followerVertex = persister.storeUser(graph, follower);
-                                        graph.commit();
-
-                                        final Iterable<ODocument> execute = graph.command(new OSQLSynchQuery<>("select from " + followerVertex.getId() + " where out('Follows').@rid contains " + user.getId()))
-                                                .execute();
-
-                                        if (!execute.iterator().hasNext()) {
-                                            graph.addEdge("class:Follows", followerVertex, user, null);
-                                        }
-                                        graph.commit();
-                                        return follower.getScreenName();
+                                    if (!execute.iterator().hasNext()) {
+                                        graph.addEdge("class:Follows", followerVertex, user, null);
                                     }
-                            ).collect(Collectors.joining(", "));
+                                    graph.commit();
+                                    return follower.getScreenName();
+                                }
+                        ).collect(Collectors.joining(", "));
 
-                    log.info("followers of user {} :: {} ", user, followers);
-                } catch (TwitterException e) {
-                    handleRetryAfter(e);
-                }
-
-                try {
-                    final String friends = followersResources.getFriendsList(userId, -1, 200).stream()
-                            .map(friend -> {
-                                        Vertex vertex = persister.storeUser(graph, friend);
-
-                                        graph.commit();
-                                        final Iterable<ODocument> execute = graph.command(new OSQLSynchQuery<>("select from " + vertex.getId() + " where out('Follows').@rid contains " + user.getId()))
-                                                .execute();
-
-                                        if (!execute.iterator().hasNext()) {
-                                            graph.addEdge("class:Follows", vertex, user, null);
-                                        }
-                                        graph.commit();
-                                        return friend.getScreenName();
-                                    }
-                            ).collect(Collectors.joining(","));
-
-                    log.info("friends of user {} :: {} ", user, friends);
-
-                } catch (TwitterException e) {
-                    handleRetryAfter(e);
-                }
-
-                log.info("done for user {} ", userId);
-            } catch (Exception e) {
-                log.error("something went wrong ", e);
-            } finally {
-                graph.shutdown();
+                log.info("followers of user {} :: {} ", user, followers);
+            } catch (TwitterException e) {
+                handleRetryAfter(e);
             }
 
-            return true;
-        }
+            try {
+                final String friends = followersResources.getFriendsList(userId, -1, 200).stream()
+                        .map(friend -> {
+                                    Vertex vertex = persister.storeUser(graph, friend);
 
-        public void handleRetryAfter(TwitterException e) {
-            if (e.getStatusCode() == 500 || e.exceededRateLimitation()) {
-                RateLimitStatus limitStatus = e.getRateLimitStatus();
+                                    graph.commit();
+                                    final Iterable<ODocument> execute = graph.command(new OSQLSynchQuery<>("select from " + vertex.getId() + " where out('Follows').@rid contains " + user.getId()))
+                                            .execute();
 
-                log.warn("rate limit exceeded, wait for {} seconds", limitStatus.getSecondsUntilReset());
-                try {
-                    TimeUnit.SECONDS.sleep(limitStatus.getSecondsUntilReset());
-                } catch (InterruptedException e1) {
-                    //nooop
-                }
+                                    if (!execute.iterator().hasNext()) {
+                                        graph.addEdge("class:Follows", vertex, user, null);
+                                    }
+                                    graph.commit();
+                                    return friend.getScreenName();
+                                }
+                        ).collect(Collectors.joining(","));
 
-            } else {
-                log.error(e);
+                log.info("friends of user {} :: {} ", user, friends);
+
+            } catch (TwitterException e) {
+                handleRetryAfter(e);
             }
+
+            log.info("done for user {} ", userId);
+        } catch (Exception e) {
+            log.error("something went wrong ", e);
+        } finally {
+            graph.shutdown();
         }
 
-        @Override
-        public void end() {
+        return true;
+    }
 
-        }
+    private void handleRetryAfter(TwitterException e) {
+        if (e.getStatusCode() == 500 || e.exceededRateLimitation()) {
+            RateLimitStatus limitStatus = e.getRateLimitStatus();
 
-        @Override
-        public Object getResult() {
-            return null;
+            log.warn("rate limit exceeded, wait for {} seconds", limitStatus.getSecondsUntilReset());
+            try {
+                TimeUnit.SECONDS.sleep(limitStatus.getSecondsUntilReset());
+            } catch (InterruptedException e1) {
+                //nooop
+            }
+
+        } else {
+            log.error(e);
         }
     }
+
 
 }
